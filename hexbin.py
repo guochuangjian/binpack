4da3f009101f4dd00257a154f71b18bcee1e8cab
guochuangjian <465469791@qq.com>
2018-12-04 21:09:03 +0800

    fix some bug

--- hexbin.py
+++ hexbin.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import struct
 
 #intel-hex 格式说明
 #:LLAAAARRDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDZZ
@@ -13,30 +14,40 @@ import sys
 # - 02 扩展段地址记录 (paragraph record)
 # - 03 转移地址记录 (transfer address record)
 # - 04 扩展线性地址记录 (expand address record)
+# - 05 开始线性地址记录
 #DD——16byte数据
 #ZZ——校验
 
 def hex2bin(hex_file, bin_file):
-    with open(hex_path, 'rb') as hex_fd, \
-        open(bin_path, 'wb') as bin_fd:
+    del_bin = 0
+    with open(hex_file, 'rb') as hex_fd, \
+        open(bin_file, 'wb') as bin_fd:
         for hex_str in hex_fd.readlines():
             hex_str = hex_str.strip()  #remove '\n\r\t'
             size    = int(hex_str[1:3], 16)
             dtype   = int(hex_str[7:9], 16)
             if dtype == 0: #Data
-                data = []
                 for i in range(size):
-                    data += int(hex_str[(10 + i * 2):(10 + i * 2 + 2)], 16)
-                bin_fd.write(data)
+                    bin_fd.write(struct.pack('B', int(hex_str[(9 + i * 2):(9 + i * 2 + 2)], 16)))
             elif dtype == 1: #End
+                print("End")
                 break
+            elif dtype == 2: 
+                print("[HEX86]:Expand addr: 0x%0X" % (int(hex_str[9 : 13], 16) << 4))
+            elif dtype == 3: 
+                print("[HEX86]Start addr : 0x%X" % (int(hex_str[9:17], 16)))
             elif dtype == 4: #Start
-                print("expand addr: 0x%x" % ((int(hex_str[10:12], 16) << 16) | 
-                                                (int(hex_str[12:14]))))
+                print("[HEX386]:Expand addr: 0x%X" % ((int(hex_str[9 : 13], 16) << 16)))
+            elif dtype == 5: 
+                print("[HEX386]Start addr : 0x%X" % (int(hex_str[9:17], 16)))
             else:
-                print("hex format error!")
+                print("hex format error, data type %d!" % dtype)
+                del_bin = 1
                 break
+    if del_bin :
+        os.unlink(bin_file)
 
+#[HEX386]
 def bin2hex(bin_file, addr, hex_file):
     with open(bin_file, 'rb') as bin_fd, \
         open(hex_file, 'wb') as hex_fd:
@@ -47,27 +58,34 @@ def bin2hex(bin_file, addr, hex_file):
             base_addr = addr >> 16 
             buf  = [0x02, 0x00, 0x00, 0x04, base_addr >> 8, base_addr & 0xFF]
         buf.append(0x100 - sum(buf))
-        f_hex.writelines(':' + ''.join(['%02x'.upper() % tmp for tmp in buf]) + '\r\n')
+        hex_fd.writelines(':' + ''.join(['%02x'.upper() % tmp for tmp in buf]) + '\r\n')
 
         #write data
         offset = addr % (64 * 1024)
         wr_addr = 0
         while True:
-            line = f_bin.read(16)
+            line = bin_fd.read(16)
             if len(line) <= 0:
                 break
-            #write data
-            f_hex.writelines(data2hexline(wr_addr, line))
+
+            #ready line
+            dbuf = [ord(val) for val in line]
+            buf  = [len(line) & 0xFF, (wr_addr >> 8) & 0xFF, wr_addr & 0xFF, 0x00]
+            buf += dbuf
+            buf.append((0x100 - (sum(buf) & 0xFF)) & 0xFF)
+
+            #write data line
+            hex_fd.writelines(':' + ''.join('%02X'.upper() % val for val in buf) + '\r\n')
         
             offset += len(line)
-            if (start + offset) % (64 * 1024) == 0:
-                print("new addr : %08x" % (start + offset))
+            if (addr + offset) % (64 * 1024) == 0:
+                print("new addr : %08x" % (addr + offset))
                 #write new line
                 wr_addr = 0
-                addr = (start + offset) >> 16 
+                addr = (addr + offset) >> 16 
                 buf  = [0x02, 0x00, 0x00, 0x04, (addr >> 8) & 0xFF, addr & 0xFF]
                 buf.append((0x100 - (sum(buf) & 0xFF)) & 0xFF)
-                f_hex.writelines(':' + ''.join(['%02x'.upper() % tmp for tmp in buf]) + '\r\n')
+                hex_fd.writelines(':' + ''.join(['%02x'.upper() % tmp for tmp in buf]) + '\r\n')
             else:
                 wr_addr += len(line)  
 
@@ -84,7 +102,7 @@ def bin_combine(bin1_file, bin2_file, bin2_addr, output_bin, padding_byte = 0xFF
             print("Error:bin1 file len >= bin2 addr!")
             os.unlink(output_bin)
 
-        output_fd.write(padding_byte * (bin2_addr - bin1_len))
+        output_fd.write(struct.pack('B', padding_byte) * (bin2_addr - bin1_len))
         output_fd.write(bin2_fd.read())
         
 if __name__ == "__main__":
@@ -95,16 +113,20 @@ if __name__ == "__main__":
     print("test hex2bin ok!")
 
     print("test bin to hex file!")
+ 
+    # bin_file = "./bb.bin"
+    # hex_file = "./output_hh.hex"
+    # offset  = 0x8000000
     bin_file = input("bin file path:")
     hex_file = input("generate hex file path:")
     offset   = input("hex file offset:")
     bin2hex(bin_file, offset, hex_file)
     print("test bin2hex ok!")
 
-    print("test bin combine!")
-    bin1_file = input("bin1 file path:")
-    bin2_file = input("bin2 file path:")
-    bin_file  = input("combine file path:")
-    addr      = input("bin2 file addr:")
-    bin_combine(bin1_file, bin2_file, addr, bin_file)
+    # print("test bin combine!")
+    # bin1_file = input("bin1 file path:")
+    # bin2_file = input("bin2 file path:")
+    # bin_file  = input("combine file path:")
+    # addr      = input("bin2 file addr:")
+    # bin_combine(bin1_file, bin2_file, addr, bin_file)
      
