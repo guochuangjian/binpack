88df74d42881b820e7ca6f450fc370114bb585b4
guochuangjian <465469791@qq.com>
2018-12-24 23:31:27 +0800

    add prefix fmt

--- structpack.py
+++ structpack.py
@@ -115,26 +115,32 @@ class structpack(object):
 
     def __struct_info_gen(self, s_key, info):
         struct_info = {} 
-        for key, value in info.items():
-            struct_info[key] = {}
-            struct_info[key]["name"] = key
-            # struct_info[key]["type"] = value[value.find('[') + 1: value.find(']')]
+        for member, value in info.items():
+            struct_info[member] = {}
+            struct_info[member]["name"] = member 
             num = 1
             if '[' in value:
                 #截取数组大小
                 num = int(value[value.find('[') + 1: value.find(']')])
                 value = value[:value.find('[')]
-            struct_info[key]["type"] = value
-            struct_info[key]["num"]  = num
-            struct_info[key]["size"] = self.__type2size(value)
-            struct_info[key]["fmt"]  = self.__type2fmt(value)
-        self.__s_dict[s_key]["struct"] = struct_info
+            if value not in types.keys():
+                print("member type not define!")
+                return
+            struct_info[member]["type"] = value
+            struct_info[member]["num"]  = num
+            struct_info[member]["size"] = self.__type2size(value)
+            struct_info[member]["fmt"]  = (str(num) if num > 1 else '') + self.__type2fmt(value)
+        self.__s_dict[s_key] = struct_info
 
     def __val_convert(self, value_info, value):
         value_type = value_info["type"]
         num        = value_info["num"]
         if value_type == "string":
-            return value #fixme 填充到指定长度
+            if num > len(value):
+                value = value + '\0' * (num - len(value))
+            else:
+                value = value[:num]
+            return value
         elif value_type == "float" or value_type == "double":
             if num == 1:
                 return float(value)
@@ -160,16 +166,9 @@ class structpack(object):
 
     def __value_gen(self, d_key, dd):
         for key, value in dd.items():
-            value_info = self.__s_dict[d_key]["struct"][key] 
+            value_info = self.__s_dict[d_key][key] 
             data = self.__val_convert(value_info, value)
-            self.__s_dict[d_key]["struct"][key].update({"value":data})
-
-    def __struct_fmt_gen(self, s_key):
-        struct_info = self.__s_dict[s_key]["struct"]
-        struct_fmt = ''
-        for member, info in struct_info.items():
-            struct_fmt = struct_fmt + info["fmt"] * info["num"] 
-        self.__s_dict[s_key]["fmt"] = struct_fmt
+            self.__s_dict[d_key][key].update({"value":data})
 
     def __convert(self):
         self.__s_dict = {}
@@ -179,11 +178,7 @@ class structpack(object):
         ss = self.__s_info[STRUCTURE] #struct info
         dd = self.__s_info[DEFINES]   #struct defines
         for s_key in ss.keys():  #s_key: struct define
-            self.__s_dict[s_key] = {}
-            # generate "struct" info
             self.__struct_info_gen(s_key, ss[s_key])
-            self.__struct_fmt_gen(s_key)
-
         #defines find
         for d_key in dd.keys():
             if d_key not in self.__s_dict.keys():
@@ -192,37 +187,39 @@ class structpack(object):
             self.__value_gen(d_key, dd[d_key])
 
     def fmt(self, struct_name):
-        return self.__s_dict[struct_name]["fmt"]
+        fmt = ''
+        for member, info in self.__s_dict[struct_name].items():
+           fmt = fmt + info["fmt"]
+        return fmt
 
-    def value(self, struct_name):
+    def value(self, struct_name, prefix_fmt = ''):
         if struct_name not in self.__s_dict.keys():
             return None
-        struct_info = self.__s_dict[struct_name]["struct"]
         values = []
-        for member, info in struct_info.items():
+        for member, info in self.__s_dict[struct_name].items():
             value = info["value"]
             if type(value) == str:
                 for i in range(len(value)):
                     values.append(bytes(value[i], encoding="utf-8"))
-            elif type(value) == tuple:
+            elif type(value) == tuple or type == list:
                 for data in value:
                     values.append(data)
             elif type(value) == int or type(type) == float:
                 values.append(value)
             else:
                 print("data type error!")
-        s = struct.Struct(self.fmt(struct_name))
-        print(len(values))
-        print(self.fmt(struct_name))
-        print(values)
+        s = struct.Struct(prefix_fmt + self.fmt(struct_name))
         return s.pack(*tuple(values))         
 
 if __name__ == "__main__":
     path = "./samples/demo.json"
 #    print(struct_pack(path))
+
     with open(path, "r") as fd:
         json_str = fd.read()
     binhead = structpack(json_str)
+    print(binhead.fmt("bin_head"))
     with open("bindata.bin", "wb") as fd:
-        fd.write(binhead.value("bin_head"))
-    print(binhead.value("bin_head"))
+        data = binhead.value("bin_head")
+        fd.write(data)
+        print(data)
